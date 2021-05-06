f913d74a9a4771be26df0c899a00e4583fae9772
lizhimiao <zhi@miao.li>
2021-05-06 10:37:17 +0800

    修改Feed模板中category格式，修改Episode拼写错误

--- Fetch.py
+++ Fetch.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 from jinja2 import Template
+from datetime import datetime
 
 
 # rss feed xml get from https://2021.jackbarber.co.uk/blog/2017-02-14-podcast-rss-feed-template
@@ -20,19 +21,19 @@ feed_template = """<?xml version="1.0" encoding="UTF-8"?>
 </itunes:owner>
 <itunes:explicit>no</itunes:explicit>
 <itunes:image href="https://cdn.vistopia.com.cn/1535342540956.png" />
-<itunes:category text="人文"/></itunes:category>
-{% for eposide in eposides %}
+<itunes:category>人文</itunes:category>
+{% for episode in episodes %}
 <item>
-    <title>{{eposide['title']}}</title>
-    <itunes:summary>{{eposide['eposideDetail']['share_desc']}}</itunes:summary>
-    <description><![CDATA[ {{eposide['eposideDetail']['share_desc']}} <br> 标签:{{eposide['eposideDetail']['tag']}} <br>{{eposide['eposideDetail']['content'] | safe}}]]> </description>
-    <link>{{eposide['share_url']}}</link>
-    <enclosure url="{{eposide['media_key_full_url']}}" type="audio/mpeg" length="{{eposide['media_size']}}"></enclosure>
-    <pubDate>{{eposide['eposideDetail']['update_date'] | replace(".","/")}}</pubDate>
+    <title>{{episode['title']}}</title>
+    <itunes:summary>{{episode['episodeDetail']['share_desc']}}</itunes:summary>
+    <description><![CDATA[ {{episode['episodeDetail']['share_desc']}} <br> 标签:{{episode['episodeDetail']['tag']}} <br>{{episode['episodeDetail']['content'] | safe}}]]> </description>
+    <link>{{episode['share_url']}}</link>
+    <enclosure url="{{episode['media_key_full_url']}}" type="audio/mpeg" length="{{episode['media_size']}}"></enclosure>
+    <pubDate>{{episode['pubDate']}}</pubDate>
     <itunes:author>梁文道</itunes:author>
-    <itunes:duration>{{eposide['duration_str']}}</itunes:duration>
+    <itunes:duration>{{episode['duration_str']}}</itunes:duration>
     <itunes:explicit>no</itunes:explicit>
-    <guid>{{eposide['article_id']}}</guid>
+    <guid>{{episode['article_id']}}</guid>
 </item> 
 {% endfor %}
 </channel>
@@ -40,35 +41,38 @@ feed_template = """<?xml version="1.0" encoding="UTF-8"?>
 """
 
 
-def fetchEposides():
+def fetchepisodes():
     req = requests.session()
     action = "https://api.vistopia.com.cn/api/v1/content/article_list?api_token=null&content_id=6&api_token=null&count=1001"
     resp = req.get(action)
-    eposides = []
+    episodes = []
     if resp.status_code == 200:
         podcasts_data = resp.json()
-        for eposide in podcasts_data['data']['article_list']:
-            eposideDetail = fetchEposideDetail(eposide['article_id'])
-            print("fething eposides"+eposide['title'])
-            eposide['eposideDetail'] = eposideDetail['data']['part'][0]
-            eposides.append(eposide)
-        #print(eposides)
-        return eposides
+        for episode in podcasts_data['data']['article_list']:
+            episodeDetail = fetchepisodeDetail(episode['article_id'])
+            print("fething episodes"+episode['title'])
+            episode['pubDate'] = datetime.fromtimestamp(int(episode['media_key'][:-4])/1000).strftime("%a, %d %b %Y %H:%M:%S %z")
+            # guess real update time from filename, media_key field.
+            episode['episodeDetail'] = episodeDetail['data']['part'][0]
+            # attach essential episode detail to episode params.
+            episodes.append(episode)
+        #print(episodes)
+        return episodes
     else:
         sys.exit()
     pass
 
-def fetchEposideDetail(eposideID):
+def fetchepisodeDetail(episodeID):
     # 抓取每篇文章描述
-    resp = requests.get("https://api.vistopia.com.cn/api/v1/reader/section-detail?api_token=&article_id="+eposideID)
+    resp = requests.get("https://api.vistopia.com.cn/api/v1/reader/section-detail?api_token=&article_id="+episodeID)
     if resp.status_code == 200:
         return resp.json()
     else:
         sys.exit()
 
 def main():
-    eposides = fetchEposides()
-    xml = Template(feed_template).render(eposides = eposides)
+    episodes = fetchepisodes()
+    xml = Template(feed_template).render(episodes = episodes)
     with open("feed.xml",'w',encoding='utf-8') as xmlFile:
         xmlFile.write(xml)
     print("Done.")
